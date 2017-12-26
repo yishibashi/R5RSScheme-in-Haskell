8bb7bef1e0c93236d727643defe909684f9f9df0
Yoshihiro Ishibashi <yishibashi101@gmail.com>
2017-12-27 01:06:48 +0900

    chap01: add code, exercise

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
-import Lib
+import           Lib
+import           Prelude
+import           System.Environment
+
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    putStrLn ("Hello, " ++ args !! 0)
+
+    {-- Exercises --}
+
+    let args1 = args !! 0
+    let args2 = args !! 1
+    let val1 = read args1
+    let val2 = read args2
+
+    putStrLn ("args: " ++ args1 ++ " " ++ args2)
+
+    putStrLn (args1 ++ " + " ++ args2 ++ " = " ++ show (val1 + val2 ))
+
+    putStrLn "Tell me your name."
+    name <- getLine
+    putStrLn ("Hi, " ++ name ++ "!")
+
