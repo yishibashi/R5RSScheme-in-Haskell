e60fb02fc1c7bc4df9f11a36c72c196d2598c916
Yoshihiro Ishibashi <yishibashi101@gmail.com>
2018-01-02 17:39:04 +0900

    add chap03:Evaluator (Exercises

--- app/Main.hs
+++ app/Main.hs
@@ -33,6 +33,11 @@ showVal :: LispVal -> String
 showVal (String contents)  = "\"" ++ contents ++ "\""
 showVal (Atom name)        = name
 showVal (Number contents) = show contents
+{-
+showVal (Float contents) = show contents
+showVal (Ratio contents) = show contents
+showVal (Complex contents) = show contents
+-}
 showVal (Bool True)        = "#t"
 showVal (Bool False)       = "#f"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
@@ -45,6 +50,11 @@ unwordsList = unwords . map showVal
 eval :: LispVal -> LispVal
 eval val@(String _)             = val
 eval val@(Number _)             = val
+{-
+eval val@(Float _)              = val
+eval val@(Ratio _)              = val
+eval val@(Complex _)            = val
+-}
 eval val@(Bool _)               = val
 eval (List [Atom "quote", val]) = val
 eval (List (Atom func : args))  = apply func $ map eval args
@@ -59,18 +69,52 @@ primitives = [("+", numericBinop (+)),
               ("/", numericBinop div),
               ("mod", numericBinop mod),
               ("quotient", numericBinop quot),
-              ("remainder", numericBinop rem)]
+              ("remainder", numericBinop rem),
+              ("symbol?", unaryOp symbolp),
+              ("string?", unaryOp stringp),
+              ("number?", unaryOp numberp),
+              ("bool?", unaryOp boolp),
+              ("list?", unaryOp listp),
+              ("symbol->string", unaryOp symbol2string),
+              ("string->symbol", unaryOp string2symbol)]
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
 numericBinop op params = Number $ foldl1 op $ map unpackNum params
 
 unpackNum (Number n) = n
+{--
 unpackNum (String n) = let parsed = reads n in
                            if null parsed
                             then 0
                             else fst $ parsed !! 0
-unpackNum (List [n]) = unpackNum n
-unpackNum _ = 0
+                            --}
+-- unpackNum (List [n]) = unpackNum n
+unpackNum _          = 0
+
+
+unaryOp :: (LispVal -> LispVal) -> [LispVal] -> LispVal
+unaryOp op [v] = op v
+
+
+symbolp, numberp, stringp, boolp, listp :: LispVal -> LispVal
+symbolp (Atom _) = Bool True
+symbolp _        = Bool False
+numberp (Number _) = Bool True
+numberp _          = Bool False
+tringp (String _) = Bool True
+stringp _          = Bool False
+boolp   (Bool _) = Bool True
+boolp   _        = Bool False
+listp   (List _)         = Bool True
+listp   (DottedList _ _) = Bool True
+listp   _                = Bool False
+symbol2string :: LispVal -> LispVal
+symbol2string (Atom s) = (String s)
+symbol2string _        = String ""
+string2symbol :: LispVal -> LispVal
+string2symbol (String a) = (Atom a)
+string2symbol _          = Atom ""
+
 
 
 {-- +-+-+-+-+-+-+-+-+-+-+ Evaluator> +-+-+-+-+-+-+-+-+-+-+ --}
